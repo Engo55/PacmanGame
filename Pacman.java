6517ad966d0a98077851f64a267812120f0341d9
Engo55 <engin655@abv.bg>
2022-02-04 13:18:00 +0200

    Added Pacman game project. For now it's nothing much.

---
+++ Pacman.java
@@ -0,0 +1,35 @@
+package pacman.game;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Pacman extends JFrame {
+
+    public Pacman() {
+
+        initUI();
+
+    }
+
+    private void initUI() {
+        add(new Board());
+
+        setTitle("Pacman");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(380,420);
+        setLocationRelativeTo(null);
+
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+
+            Pacman ex = new Pacman();
+            ex.setVisible(true);
+
+            String filepath = "D:\\PacmanGame\\Pacman_Pics\\pacmanMusic.wav";
+            Board musicObject = new Board();
+            musicObject.playMusic(filepath);
+        });
+    }
+}
